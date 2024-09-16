39512a53ffca02f0b22526e86f7bc799b984c6fa
ast <astrznova@gmail.com>
2024-09-16 15:28:26 +0300

    Тесты

--- src/card.py
+++ src/card.py
@@ -1,14 +1,53 @@
-"""Карты UNO."""
-
-
 class Card:
     def __init__(self, color: str, number: int):
         self.color = color
         self.number = number
+        # self.rank = self.calculate_rank()
 
     def __repr__(self):
-        # 'r3'
         return f'{self.color}{self.number}'
 
     def save(self):
         return repr(self)
+
+    # def calculate_rank(self) -> int:
+    #     # Определяем ранг карты
+    #     if self.number % 5 == 0 and self.number % 11 == 0:  # 55
+    #         return 7  # фиолетовая карта
+    #     elif self.number % 5 == 0:
+    #         return 2  # голубые карты (кратные 5)
+    #     elif self.number % 10 == 0:
+    #         return 3  # желтые карты (кратные 10)
+    #     elif self.number % 11 == 0:  # двойные карты: 11, 22, 33...
+    #         return 5  # красные карты
+    #     else:
+    #         return 1  # зеленые карты (все остальные)
+
+# Дополнительные методы для взаимодействия с правилами игры
+    def can_place_in_row(self, last_card: 'Card'):
+        """Проверка, можно ли положить текущую карту в ряд."""
+        return self.number > last_card.number
+
+
+# Пример использования класса
+if __name__ == "__main__":
+    card1 = Card('g', 3)
+    card2 = Card('b', 5)
+    card3 = Card('y', 10)
+    card4 = Card('r', 11)
+    card5 = Card('p', 55)
+
+    print(card1, card1)  # g3 1
+    print(card2, card2)  # b5 2
+    print(card3, card3)  # y10 3
+    print(card4, card4)  # r11 5
+    print(card5, card5)  # p55 7
+
+    # print(card1, card1.rank)  # g3 1
+    # print(card2, card2.rank)  # b5 2
+    # print(card3, card3.rank)  # y10 3
+    # print(card4, card4.rank)  # r11 5
+    # print(card5, card5.rank)  # p55 7
+
+    # Проверка на возможность размещения
+    print(card1.can_place_in_row(card2))
--- test/test_card.py
+++ test/test_card.py
@@ -1,4 +1,5 @@
 from src.card import Card
+import pytest
 
 
 def test_init():
@@ -15,4 +16,33 @@ def test_save():
     c = Card('g', 7)
     assert repr(c) == 'g7'
     assert c.save() == 'g7'
+
+    c = Card('v', 55)
+    assert repr(c) == 'y3'
+    assert c.save() == 'y3'
+
+
+def test_validation():
+    with pytest.raises(ValueError):
+        Card('yellow', 1)
+    with pytest.raises(ValueError):
+        Card('ф', 1)
+    with pytest.raises(ValueError):
+        Card('b', 10)
+    with pytest.raises(ValueError):
+        Card('b', 3)
+
+
+# def test_play_on():
+#     c1 = Card.load('y1')
+#     c2 = Card.load('y5')
+#     c3 = Card.load('g1')
+#     c4 = Card.load('g6')
+#
+#     assert c1.can_play_on(c1)
+#     assert c2.can_play_on(c1)
+#     assert c1.can_play_on(c2)
+#     assert c3.can_play_on(c1)
+#     assert not c4.can_play_on(c1)
+
 
