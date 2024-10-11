93cc0fc8d70cece043bd012161afcf910c0c4c07
ast <astrznova@gmail.com>
2024-10-12 01:13:33 +0300

    Cdeck

---
+++ src/Cdeck.py
@@ -0,0 +1,49 @@
+import random
+from src.Ccard import Card
+
+
+class Deck:
+    def __init__(self, cards: None | list[Card]):
+        if cards is None:
+            cards = Card.all_cards()
+            random.shuffle(cards)
+        self.cards: list[Card] = cards
+
+    def __repr__(self) -> str:
+        return self.show_deck()
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, str):
+            other = Deck.load(other)
+        return self.cards == other.cards
+
+    def save(self) -> str:
+        """Сохраняет колоду в простом текстовом формате."""
+        return ' '.join(card.save() for card in self.cards)
+
+    @classmethod
+    def load(cls, text: str) -> 'Deck':
+        cards = []
+        for s in text.split():
+            if s.startswith('[') and s.endswith(']'):
+                try:
+                    cards.append(Card.load(s[1:-1]))
+                except Exception as e:
+                    print(f"Ошибка загрузки карты {s}: {e}")
+        return cls(cards=cards)
+
+    def draw_card(self):
+        """Берет карту из колоды и возвращает ее."""
+        return self.cards.pop() if self.cards else None
+
+    def full_deck(self):
+        """Возвращает всю колоду."""
+        return self.cards
+
+    def shuffle(self):
+        """Перемешивает колоду."""
+        random.shuffle(self.cards)
+
+    def show_deck(self) -> str:
+        """Возвращает строковое представление колоды с цветами карт."""
+        return ' '.join(repr(card) for card in self.cards)
