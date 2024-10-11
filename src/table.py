d11ce2e34ac76b5c4cbb56f229e3e60fb51542fd
ast <astrznova@gmail.com>
2024-10-12 00:46:53 +0300

    table + test (в table добавлен выбор ряда игроком)

--- src/table.py
+++ src/table.py
@@ -13,7 +13,7 @@ class Table:
         best_difference = float('inf')
 
         for i, row in enumerate(self.rows):
-            if not row.is_full():
+            if len(row.cards) < Row.MAX_CARDS:
                 if row.cards:
                     last_card = row.cards[-1]
                     difference = card.number - last_card.number
@@ -26,19 +26,25 @@ class Table:
 
         if best_row_index is not None:
             row = self.rows[best_row_index]
-            if len(row.cards) == 5:
+            if len(row.cards) == Row.MAX_CARDS - 1:
                 points = row.score()
                 row.clear()
                 row.add_card(card)
                 print(f"\n Игрок забрал ряд {best_row_index + 1}. Получено очков: {points}")
+                return True
             else:
                 return row.add_card(card)
-        return False  # Если нет доступного ряда, игрок выберет ряд, который заберет.
+        else:
+            chosen_row = self.choose_row()
+            points = chosen_row.score()
+            chosen_row.clear()
+            print(f"\nИгрок забрал ряд {self.rows.index(chosen_row) + 1}. Получено очков: {points}")
+            chosen_row.add_card(card)
 
     def choose_row(self):
         while True:
             try:
-                chosen_row_index = int(input("Выберите ряд для забирания (1-4): ")) - 1
+                chosen_row_index = int(input("Выберите ряд (1-4): ")) - 1
                 if 0 <= chosen_row_index < len(self.rows):
                     return self.rows[chosen_row_index]
                 else:
@@ -50,17 +56,58 @@ class Table:
         return self.rows[index]
 
     def save(self) -> str:
-        return json.dumps([[card.to_dict() for card in row.cards] for row in self.rows])
+        return json.dumps([[[card.number, card.rank] for card in row.cards] for row in self.rows])
 
     @classmethod
-    def load(cls, rows_data: list[str]):
+    def load(cls, rows_data: str):
         table = cls()
-        for i, row_data in enumerate(rows_data):
-            cards = Card.load_multiple(row_data)
-            for card in cards:
+        loaded_rows = json.loads(rows_data)
+        for i, row_data in enumerate(loaded_rows):
+            for card_data in row_data:
+                card = Card(card_data[0])
                 table.rows[i].add_card(card)
         return table
 
     def __repr__(self):
         repr_rows = [f"r{i + 1}: {repr(row)}" for i, row in enumerate(self.rows)]
         return "\n".join(repr_rows)
+
+
+#проверка функции выбора пользователем ряда
+if __name__ == "__main__":
+    table = Table()
+    test_cards = [
+            [Card(1), Card(8), Card(9)],
+            [Card(2), Card(7), Card(10)],
+            [Card(3), Card(6), Card(11)],
+            [Card(4), Card(5), Card(12), Card(13), Card(14)]
+        ]
+
+    for i in range(4):
+        for card in test_cards[i]:
+            table.rows[i].add_card(card)
+    print(table)
+
+    chosen_card = Card(15)
+    table.add_card(chosen_card)
+    print(f"<Игрок> выбрал карту {chosen_card}")
+    print("\nТекущиее состояние стола:")
+    print(table)
+
+
+#1  Добавление 5-й карты в ряд
+    # test_cards = [
+    #     [Card(1), Card(8), Card(9)],
+    #     [Card(2), Card(7), Card(10)],
+    #     [Card(3), Card(6), Card(11)],
+    #     [Card(4), Card(5), Card(12), Card(13), Card(14)]
+    # ]
+    #     table.add_card(Card(15))
+#2  Добавление карты меньшей всех карт
+    #     [Card(1), Card(82)],
+    #     [Card(2), Card(7), Card(104)],
+    #     [Card(3), Card(61)],
+    #     [Card(49)]
+    # ]
+    #     table.add_card(Card(40))
+
--- test/test_table.py
+++ test/test_table.py
@@ -10,27 +10,34 @@ def test_table_init():
         assert isinstance(row, Row)
 
 
+def add_card_to_row(table: Table, card: Card):
+    for _ in table.rows:
+        if table.add_card(card):
+            return True
+    return False
+
+
 def test_add_card():
     table = Table()
 
     # Добавление карт
-    table.add_card(Card(10))  # добавляется в 1 ряд
-    table.add_card(Card(25))  # во 2 ряд
-    table.add_card(Card(41))  # в 3 ряд
-    table.add_card(Card(73))  # в 4 ряд
-    table.add_card(Card(55))  # в 3 ряд
-    table.add_card(Card(76))  # в 4 ряд (после 73)
-    table.add_card(Card(77))  # в 4 ряд (после 76)
-    table.add_card(Card(83))  # в 4 ряд (после 77)
-    table.add_card(Card(90))  # в 4 ряд (после 83)
+    add_card_to_row(table, Card(10))  # добавляется в 1 ряд
+    add_card_to_row(table, Card(25))  # во 2 ряд
+    add_card_to_row(table, Card(41))  # в 3 ряд
+    add_card_to_row(table, Card(73))  # в 4 ряд
+    add_card_to_row(table, Card(55))  # в 3 ряд
+    add_card_to_row(table, Card(76))  # в 4 ряд (после 73)
+    add_card_to_row(table, Card(77))  # в 4 ряд (после 76)
+    add_card_to_row(table, Card(83))  # в 4 ряд (после 77)
+    add_card_to_row(table, Card(90))  # в 4 ряд (после 83)
 
     assert len(table.get_row(0).cards) == 1
     assert len(table.get_row(1).cards) == 1
     assert len(table.get_row(2).cards) == 2
     assert len(table.get_row(3).cards) == 5
 
-    #table.add_card(Card(91))  # 4 ряд очистился
-    #assert len(table.get_row(3).cards) == 1
+    add_card_to_row(table, Card(91))  # 4 ряд очистится. игрок его забрал
+    assert len(table.get_row(3).cards) == 1
 
 
 def test_save_load():
