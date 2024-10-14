6ab29471365fe312c5fcea5975fbc9946f50f7c6
ast <astrznova@gmail.com>
2024-10-15 00:22:21 +0300

    table + tests

--- test/test_table.py
+++ test/test_table.py
@@ -1,6 +1,7 @@
 from src.card import Card
 from src.row import Row
 from src.table import Table
+import json
 
 
 def test_table_init():
@@ -10,34 +11,30 @@ def test_table_init():
         assert isinstance(row, Row)
 
 
-def add_card_to_row(table: Table, card: Card):
-    for _ in table.rows:
-        if table.add_card(card):
-            return True
-    return False
-
-
 def test_add_card():
     table = Table()
 
     # Добавление карт
-    add_card_to_row(table, Card(10))  # добавляется в 1 ряд
-    add_card_to_row(table, Card(25))  # во 2 ряд
-    add_card_to_row(table, Card(41))  # в 3 ряд
-    add_card_to_row(table, Card(73))  # в 4 ряд
-    add_card_to_row(table, Card(55))  # в 3 ряд
-    add_card_to_row(table, Card(76))  # в 4 ряд (после 73)
-    add_card_to_row(table, Card(77))  # в 4 ряд (после 76)
-    add_card_to_row(table, Card(83))  # в 4 ряд (после 77)
-    add_card_to_row(table, Card(90))  # в 4 ряд (после 83)
+    table.add_card(Card(10))  # добавляется в 1 ряд
+    table.add_card(Card(25))  # во 2 ряд
+    table.add_card(Card(41))  # в 3 ряд
+    table.add_card(Card(73))  # в 4 ряд
+    table.add_card(Card(55))  # в 3 ряд
+    table.add_card(Card(76))  # в 4 ряд (после 73)
+    table.add_card(Card(77))  # в 4 ряд (после 76)
+    table.add_card(Card(83))  # в 4 ряд (после 77)
+    table.add_card(Card(90))  # в 4 ряд (после 83)
+    #print(table)
 
-    assert len(table.get_row(0).cards) == 1
-    assert len(table.get_row(1).cards) == 1
-    assert len(table.get_row(2).cards) == 2
-    assert len(table.get_row(3).cards) == 5
+    assert repr(table[0]) == '[10<3>]'
+    assert repr(table[1]) == '[25<2>]'
+    assert repr(table[2]) == '[41<1>] [55<7>]'
+    assert repr(table[3]) == '[73<1>] [76<1>] [77<5>] [83<1>] [90<3>]'
 
-    add_card_to_row(table, Card(91))  # 4 ряд очистится. игрок его забрал
-    assert len(table.get_row(3).cards) == 1
+    table.add_card(Card(91))  # 4 ряд очистится. игрок его забрал
+    assert repr(table[3]) == '[91<1>]'
+    assert len(table[3].cards) == 1
+    #print(table)
 
 
 def test_save_load():
@@ -47,12 +44,13 @@ def test_save_load():
     table.add_card(Card(25))
 
     saved_data = table.save()
-    new_table = Table.load(saved_data)
+    loaded_data = json.loads(saved_data)
+    new_table = Table.load(loaded_data)
 
-    assert new_table.get_row(0).cards[0] == Card(10)
-    assert new_table.get_row(1).cards[0] == Card(25)
-    assert len(new_table.rows[0].cards) == 1
-    assert len(new_table.rows[1].cards) == 1
+    assert new_table[0].cards[0] == Card(10)
+    assert new_table[1].cards[0] == Card(25)
+    assert len(new_table[0].cards) == 1
+    assert len(new_table[1].cards) == 1
 
 
 def test_repr():
