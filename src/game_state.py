0e94f844313eb428cf22f421c9cdd290e8ccefa8
ast <astrznova@gmail.com>
2024-10-16 21:21:54 +0300

    gamestate + tests(изменила save, load, play card, добавила определение победителя)

--- src/game_state.py
+++ src/game_state.py
@@ -1,26 +1,50 @@
-from src.table import Table
 from src.card import Card
 from src.deck import Deck
 from src.player import Player
+from src.table import Table
 
 
 class GameState:
-    def __init__(self, players: list[Player], deck: Deck, table: Table, current_player_index: int = 0):
-        self.players = players
-        self.deck = deck
-        self.table = table
-        self.current_player_index = current_player_index
+    """
+    data = {
+    "table": {
+        "row1": "[39<1>] [45<2>]",
+        "row2": "[89<1>]",
+        "row3": "[80<3>] [99<5>] [101<1>]",
+        "row4": "[77<5>]",
+    },
+    "deck": "",
+    "current_player_index": 0,
+    "players": [
+        {
+            "name": "Ast",
+            "hand": "[82<1>] [102<1>] [35<2>] [65<2>] [33<5>] [66<5>] [44<5>] [98<1>] [97<1>] [25<2>]",
+            "score": 0
+        },
+        {
+            "name": "P1",
+            "hand": "[12<1>] [71<1>] [5<2>] [81<1>] [49<1>] [21<1>] [3<1>] [85<2>] [22<5>] [26<1>]",
+            "score": 0
+        }
+    ]
+}
+    """
+
+    def __init__(
+        self, players: list[Player], deck: Deck, table: Table, current_player: int = 0
+    ):
+        self.players: list[Player] = players
+        self.deck: Deck = deck
+        self.table: Table = table
+        self._current_player: int = current_player
 
     def current_player(self) -> Player:
-        return self.players[self.current_player_index]
-
-    def next_player(self):
-        self.current_player_index = (self.current_player_index + 1) % len(self.players)
+        return self.players[self._current_player]
 
     def __eq__(self, other):
         if not isinstance(other, GameState):
             return False
-        if self.current_player_index != other.current_player_index:
+        if self._current_player != other._current_player:
             return False
         if self.players != other.players:
             return False
@@ -32,37 +56,54 @@ class GameState:
 
     def save(self) -> dict:
         return {
-            'deck': self.deck.save(),
-            'current_player_index': self.current_player_index,
-            'players': [player.save() for player in self.players],
-            'table': self.table.save()
+            "table": {f"row{i + 1}": self.table[i].save() for i in range(len(self.table.rows))},
+            "deck": str(self.deck),
+            "current_player_index": self._current_player,
+            "players": [p.save() for p in self.players],
         }
 
     @classmethod
     def load(cls, data: dict):
-        players = [Player.load(p_data) for p_data in data['players']]
-        deck = Deck.load(data['deck'])
-        table = Table.load([data['row1'], data['row2'], data['row3'], data['row4']])
-        return cls(players=players, deck=deck, table=table, current_player_index=data['current_player_index'])
+        players = [Player.load(p) for p in data["players"]]
+        table = Table.load(data["table"])
+        deck = Deck.load(data["deck"])
+        current_player = int(data["current_player_index"])
 
-    def play_card(self, card: Card):
-        self.current_player().hand.remove_card(card)
+        return cls(
+            players=players,
+            deck=deck,
+            table=table,
+            current_player=current_player,
+        )
 
-        if not self.table.add_card(card):
-            print(f"{self.current_player().name}: выбрана карта {card}")
-            chosen_row = self.table.choose_row()
-            points = chosen_row.score()
-            chosen_row.clear()
-            chosen_row.add_card(card)
-            self.current_player().score += points
-            print(f"\n{self.current_player().name} забирает ряд. Получает штрафные баллы: {points}")
+    def next_player(self):
+        """Ход переходит к следующему игроку."""
+        self._current_player = (self._current_player + 1) % len(self.players)
+
+    def play_card(self, card: Card) -> (bool, int):
+        """Текущий игрок играет карту."""
+        if card.number not in Card.NUMBERS:
+            return False, 0
+
+        if card in self.current_player().hand.cards:
+            successful, points = self.table.add_card(card)
+            if successful:
+                self.current_player().hand.remove_card(card)
+                return successful, points
         else:
-            print(f"\n{self.current_player().name}: выбрана карта {card}")
-            row = self.table
-            row.add_card(card)
+            raise ValueError(f"{self.current_player().name} не имеет карты {card} в руке.")
+        return False, 0
 
     def is_game_over(self) -> bool:
+        """Проверяет, закончилась ли игра."""
         return any(player.is_loser() for player in self.players)
 
+    def find_winner(self) -> (bool, Player or list[Player]):
+        """Находит победителя."""
+        min_score = min(player.score for player in self.players)
+        winners = [player for player in self.players if player.score == min_score]  #список игроков с минимальным количеством очков
 
-
+        if len(winners) == 1:  #если победитель 1
+            return True, winners[0]
+        else:
+            return False, winners
--- test/test_game_state.py
+++ test/test_game_state.py
@@ -1,3 +1,5 @@
+import pytest
+import json
 from src.card import Card
 from src.deck import Deck
 from src.game_state import GameState
@@ -5,113 +7,145 @@ from src.player import Player
 from src.table import Table
 
 data = {
-    'deck': ' '.join(Card(number=i).save() for i in range(1, 105)),
-    'row1': '[39<1>]',
-    'row2': '[89<1>]',
-    'row3': '[80<3>]',
-    'row4': '[77<1>]',
-    'current_player_index': 0,
-    'players': [
+    "table": {
+        "row1": "[39<1>] [45<2>]",
+        "row2": "[89<1>]",
+        "row3": "[80<3>] [99<5>] [101<1>]",
+        "row4": "[77<5>]",
+    },
+    "deck": "",
+    "current_player_index": 0,
+    "players": [
         {
-            'name': 'Ast',
-            'hand': '[82<1>]  [102<1>]  [35<2>]  [65<2>]  [33<1>]  [66<5>]  [44<5>]  [98<1>]  [97<1>]  [25<2>]',
-            'score': 0,
-            'is_human': True
+            "name": "Ast",
+            "hand": "[82<1>] [102<1>] [35<2>] [65<2>] [33<5>] [97<1>] [25<2>]",
+            "score": 0
         },
         {
-            'name': 'P1',
-            'hand': '[12<1>]  [71<1>]  [5<1>]  [81<1>]  [49<1>]  [21<1>]  [3<1>]  [85<2>]  [22<5>]  [26<1>]',
-            'score': 0,
-            'is_human': False
+            "name": "P1",
+            "hand": "[12<1>] [71<1>] [5<2>] [81<1>] [3<1>] [22<5>] [26<1>]",
+            "score": 0
         }
     ]
 }
 
-
-ast = Player.load(data['players'][0])
-p1 = Player.load(data['players'][1])
-
-rows_data = [data['row1'], data['row2'], data['row3'], data['row4']]
-table = Table.load(rows_data)
-
-deck = Deck(None)
+ast = Player.load(data["players"][0])
+p1 = Player.load(data["players"][1])
+full_deck = Deck(None)
+json_data = json.dumps(data)
+table = Table.load(json.loads(json_data)["table"])
 
 
-def test_init():
-    game = GameState(players=[ast, p1], deck=deck, table=table, current_player_index=0)
+@pytest.fixture()
+def game():
+    return GameState(players=[ast, p1], deck=full_deck, table=table, current_player=0)
+
+
+def test_init(game):
     assert game.players == [ast, p1]
-    assert game.deck == deck
-    assert game.table == table
-    assert game.current_player_index == 0
+    assert game.deck == full_deck
+    assert game.current_player().name == "Ast"
 
 
-def test_current_player():
-    game = GameState(players=[ast, p1], deck=deck, table=table, current_player_index=0)
-    assert game.current_player() == ast
+def test_current_player(game):
+    game._current_player = 0
+    assert game.current_player().name == "Ast"
+    assert game._current_player == 0
 
-    game.current_player_index = 1
-    assert game.current_player() == p1
-
-
-def test_eq():
-    game1 = GameState(players=[ast, p1], deck=deck, table=table, current_player_index=0)
-    game2 = GameState(players=[ast, p1], deck=deck, table=table, current_player_index=0)
-    game3 = GameState(players=[ast], deck=deck, table=table, current_player_index=0)
-
-    assert game1 == game2
-    assert game1 != game3
+    game._current_player = 1
+    assert game.current_player().name == "P1"
+    assert game._current_player == 1
 
 
-def test_save():
-    game = GameState(players=[ast, p1], deck=deck, table=table, current_player_index=0)
-    saved_data = game.save()
+def test_eq(game):
+    game2 = GameState(players=[ast, p1], deck=full_deck, table=table, current_player=0)
+    game3 = GameState(players=[ast, p1], deck=Deck([]), table=table, current_player=1)
+    assert game == game2
+    assert game != game3
 
-    assert saved_data['current_player_index'] == 0
-    assert len(saved_data['players']) == 2
-    assert saved_data['deck'] == deck.save()
-    assert saved_data['table'] == table.save()
+
+def test_save(game):
+    expected = {
+        "table": data["table"],
+        "deck": str(full_deck),
+        "current_player_index": 0,
+        "players": [player.save() for player in game.players],
+    }
+    assert game.save() == expected
 
 
 def test_load():
-    game = GameState.load(data)
-    assert game.current_player() == ast
+    loaded_game = GameState.load(data)
+    assert loaded_game.save() == data
 
 
-def test_next_player():
-    game = GameState(players=[ast, p1], deck=deck, table=table, current_player_index=0)
-
+def test_next_player(game):
+    assert game.current_player().name == "Ast"
     game.next_player()
-    assert game.current_player() == p1
-
+    assert game.current_player().name == "P1"
     game.next_player()
-    assert game.current_player() == ast
+    assert game.current_player().name == "Ast"
 
 
-def test_play_card():
-    game = GameState(players=[ast, p1], deck=deck, table=table, current_player_index=0)
+def test_play_card_1(game):
+    """Если карта игрока подошла"""
+    card_to_play = ast.hand.cards[0]  # ast играет картой [82<1>]
+    assert game.current_player().hand.cards  # проверка, что у ast есть карты в руке
 
-    initial_hand_size = len(game.current_player().hand.cards)
+    initial_table_state = game.table.save()  # сохр. текущее состояние стола
 
-    card_to_play = Card.load('[82<1>]')
-    game.play_card(card_to_play)
+    successful, points = game.play_card(card_to_play)
+    assert successful is True  # карта проигралась
+    assert card_to_play not in game.current_player().hand.cards  # проверяет, что карта больше не находится в руке
 
-    initial_hand_size -= 1
-
-    assert len(game.current_player().hand.cards) == initial_hand_size
-    assert table.rows[3].cards[-1] == card_to_play  # Проверка, что карта добавлена к ряду 3
-
-    card_to_play = Card(44)
-    game.play_card(card_to_play)
-
-    initial_hand_size -= 1
-
-    assert len(game.current_player().hand.cards) == initial_hand_size
-    assert table.rows[0].cards[-1] == card_to_play  # Проверка, что карта добавлена к ряду 1
+    new_table_state = game.table.save()  # проверка состояния стола после игры карты
+    assert new_table_state != initial_table_state
+    # print(new_table_state)
+    # print(game.current_player().hand.cards)
 
 
-def test_is_game_over():
-    game = GameState(players=[ast, p1], deck=deck, table=table, current_player_index=0)
+# def test_play_card_2(game):
+#     """Если карта игрока не подошла"""
+#     card_to_play = ast.hand.cards[2]  # ast играет картой [35<2>]
+#     assert game.current_player().hand.cards
+#
+#     initial_table_state = game.table.save()
+#
+#     successful, points = game.play_card(card_to_play)
+#     assert successful is False  # карта не проигралась
+#     assert card_to_play in game.current_player().hand.cards
+#
+#     new_table_state = game.table.save()
+#     assert new_table_state == initial_table_state  # состояние стола пока не изменилось
+
+
+def test_play_invalid_card(game):
+    with pytest.raises(ValueError, match="Неверный номер карты."):  # raise ValueError в классе Card
+        invalid_card = Card(999)
+        game.play_card(invalid_card)
+
+
+def test_is_game_over(game):
     assert not game.is_game_over()
 
-    ast.score = 66
+    ast.score = 128
     assert game.is_game_over()
+
+
+def test_find_winner(game):
+
+    ast.score = 65
+    p1.score = 102
+    is_winner, winner = game.find_winner()
+    assert game.is_game_over()
+    assert is_winner is True
+    assert winner.name == 'Ast'
+
+    ast.score = 71
+    p1.score = 71
+    is_winner, winners = game.find_winner()
+    assert game.is_game_over()
+    assert is_winner is False
+    assert len(winners) == 2
+    winner_names = {winner.name for winner in winners}
+    assert winner_names == {"Ast", "P1"}
