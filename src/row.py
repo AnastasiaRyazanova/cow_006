491a502e564d60b29ce26514df7f2c1ebcdedde0
ast <astrznova@gmail.com>
2024-10-16 19:35:31 +0300

    row + tests(добавила load и тесты для load и save)

--- src/row.py
+++ src/row.py
@@ -8,20 +8,32 @@ class Row:
         self.cards: list[Card] = []
 
     def add_card(self, card: Card) -> bool:
-        if not self.cards:
+        if not self.cards or card.can_play(self.cards[-1]): #если ряд пустой или карта проходит проверку(ее номер больше)
             self.cards.append(card)
-            return True
-        else:
-            last_card = self.cards[-1]
-            if card.can_play(last_card):
-                self.cards.append(card)
-                return True
-
+            return True #то можно добавить в ряд
         return False
 
     def score(self) -> int:
         """Возвращает сумму рангов карт в ряду."""
         return sum(c.cow_rank() for c in self.cards)
+
+    def save(self) -> str:
+        """Сохраняет карты в формате строки для хранения."""
+        return ' '.join(card.save() for card in self.cards)
+
+    @staticmethod
+    def load(data: str) -> 'Row':
+        """Загружает ряд из строки."""
+        row = Row()
+        cards_str = data.strip('[]')
+        if cards_str:
+            card_entries = cards_str.split('] [')
+
+            for card_entry in card_entries:
+                card = Card.load(card_entry)
+                row.add_card(card)
+
+        return row
 
     def clear(self):
         """Очищает ряд."""
--- test/test_row.py
+++ test/test_row.py
@@ -2,25 +2,47 @@ from src.card import Card
 from src.row import Row
 
 
-def test_add_card_to_empty_row():
+def test_add_card_to_row():
     row = Row()
     assert row.add_card(Card(2)) is True
     assert row.cards == [Card(2)]
 
-
-def test_add_card_with_valid_play():
     row = Row()
     row.add_card(Card(2))
     assert row.add_card(Card(3)) is True
     assert row.cards == [Card(2), Card(3)]
 
-
-def test_add_card_with_invalid_play():
     row = Row()
     row.add_card(Card(3))
     row.add_card(Card(8))
     assert row.add_card(Card(2)) is False
     assert row.cards == [Card(3), Card(8)]
+
+
+def test_save_row():
+    row = Row()
+    assert row.save() == ""
+
+    row = Row()
+    row.add_card(Card(1))
+    assert row.save() == "[1<1>]"
+
+    row = Row()
+    row.add_card(Card(1))
+    row.add_card(Card(11))
+    row.add_card(Card(55))
+    assert row.save() == "[1<1>] [11<5>] [55<7>]"
+
+
+def test_load_row():
+    row = Row.load("[]")
+    assert row.cards == []
+
+    row = Row.load("[1<1>]")
+    assert row.cards == [Card(1)]
+
+    row = Row.load("[1<1>] [11<1>] [55<7>]")
+    assert row.cards == [Card(1), Card(11), Card(55)]
 
 
 def test_total_rank():
