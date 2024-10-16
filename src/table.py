72a460ef08ebf2fc22402c54320d6007c6f29799
ast <astrznova@gmail.com>
2024-10-16 19:45:18 +0300

    table + tests(в Tableload использую Rowload, изменила сохранение save)

--- src/table.py
+++ src/table.py
@@ -12,41 +12,46 @@ class Table:
 
     def add_card(self, card: Card) -> (bool, int):
         """Добавляет карту в оптимальный ряд."""
-        for row in self.rows:
+        for row in self.rows:     #если в ряд пустой, то автоматически добавляется карта
             if not row.cards:
                 row.add_card(card)
-                return True, 0
+                return True,    0 #возвращается 0 штрафных очков
 
-        good_rows = []
+        good_rows = []    # ряды, в которые можно добавить карту
 
         for row in self.rows:
             if card.can_play(row.cards[-1]):
-                good_rows.append(row)
+                good_rows.append(row)   #добавляется в список подходящих рядов
 
-        if not good_rows:
-            return False, 0
+        if not good_rows:     #если нет подходящих рядов(номер карты меньше карт во всех рядах)
+            return False, 0      #возвращается 0 штрафных очков
 
-        attached_row = min(good_rows, key=lambda r: abs(card.number - r.cards[-1].number))
+        attached_row = min(good_rows, key=lambda r: abs(card.number - r.cards[-1].number))     #оптимальный ряд
+                                                                                               # (разница номеров минимальная)
 
-        points = 0
-        if len(attached_row.cards) == Row.MAX_CARDS - 1:
+        points = 0 #число очков, к-е затем добавятся в score игрока, забравшего ряд
+        if len(attached_row.cards) == Row.MAX_CARDS - 1:  #если карта игрока становится 6-й в ряду
             points = attached_row.score()
             print(f"\nИгрок забрал ряд {self.rows.index(attached_row) + 1}. Получено очков: {points}")
             attached_row.clear()
 
-        attached_row.add_card(card)
+        attached_row.add_card(card) #6-я карта становится 1-й в ряду
         return True, points
 
     def save(self) -> str:
-        return json.dumps([[[card.number, card.rank] for card in row.cards] for row in self.rows])
+        """Сохраняет состояние стола в виде JSON строки."""
+        return json.dumps(
+            {f"row{i + 1}": self.rows[i].save() for i in range(len(self.rows))}
+        )
 
     @classmethod
-    def load(cls, rows_data: list):
+    def load(cls, data: dict) -> 'Table':
+        """Загружает состояние стола из предоставленных данных."""
         table = cls()
-        for row_index, cards in enumerate(rows_data):
-            for card_data in cards:
-                card = Card.load(f'[{card_data[0]}<{card_data[1]}>]')
-                table[row_index].add_card(card)
+        for row_key, cards_str in data.items():
+            row = Row.load(cards_str)
+            row_index = int(row_key.replace("row", "")) - 1
+            table.rows[row_index] = row
         return table
 
     def __repr__(self):
--- test/test_table.py
+++ test/test_table.py
@@ -1,7 +1,7 @@
+import json
 from src.card import Card
 from src.row import Row
 from src.table import Table
-import json
 
 
 def test_table_init():
@@ -24,7 +24,6 @@ def test_add_card():
     table.add_card(Card(77))  # в 4 ряд (после 76)
     table.add_card(Card(83))  # в 4 ряд (после 77)
     table.add_card(Card(90))  # в 4 ряд (после 83)
-    #print(table)
 
     assert repr(table[0]) == '[10<3>]'
     assert repr(table[1]) == '[25<2>]'
@@ -34,23 +33,46 @@ def test_add_card():
     table.add_card(Card(91))  # 4 ряд очистится. игрок его забрал
     assert repr(table[3]) == '[91<1>]'
     assert len(table[3].cards) == 1
-    #print(table)
 
 
-def test_save_load():
-    """Сохранение и загрузку стола."""
+def test_save():
+    """Тестируем сохранение состояния стола."""
     table = Table()
-    table.add_card(Card(10))
-    table.add_card(Card(25))
+    table.add_card(Card(10))  # добавляется в 1 ряд
+    table.add_card(Card(25))  # во 2 ряд
+    table.add_card(Card(41))  # в 3 ряд
+    table.add_card(Card(73))  # в 4 ряд
+    table.add_card(Card(55))  # в 3 ряд
+    table.add_card(Card(76))  # в 4 ряд (после 73)
+    table.add_card(Card(77))  # в 4 ряд (после 76)
+    table.add_card(Card(83))  # в 4 ряд (после 77)
+    table.add_card(Card(90))  # в 4 ряд (после 83)
 
     saved_data = table.save()
-    loaded_data = json.loads(saved_data)
-    new_table = Table.load(loaded_data)
+    expected_data = json.dumps({
+        "row1": "[10<3>]",
+        "row2": "[25<2>]",
+        "row3": "[41<1>] [55<7>]",
+        "row4": "[73<1>] [76<1>] [77<5>] [83<1>] [90<3>]"
+    }, sort_keys=True)
 
+    assert saved_data == expected_data
+
+
+def test_load():
+    """Тестируем загрузку состояния стола."""
+    data = {
+            "row1": "[10<3>]",
+            "row2": "[25<2>]",
+            "row3": "",
+            "row4": ""
+        }
+
+    new_table = Table.load(data)
     assert new_table[0].cards[0] == Card(10)
     assert new_table[1].cards[0] == Card(25)
-    assert len(new_table[0].cards) == 1
-    assert len(new_table[1].cards) == 1
+    assert len(new_table[2].cards) == 0
+    assert len(new_table[3].cards) == 0
 
 
 def test_repr():
@@ -64,3 +86,4 @@ def test_repr():
 
     expected_repr = f"r1: [{card1.number}<{card1.rank}>]\nr2: [{card2.number}<{card2.rank}>]\nr3: \nr4: "
     assert repr(table) == expected_repr
+
